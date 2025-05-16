e80bfaf308b65267f414dd8fab27c541b8c44edb
Kamila Cisowska <cisowska.kamila0@gmail.com>
2025-05-16 15:50:05 +0200

    Adding Clickup project

---
+++ src/main/java/pl/akademiaqa/properties/ClickUpProperties.java
@@ -0,0 +1,29 @@
+package pl.akademiaqa.properties;
+
+import java.util.ResourceBundle;
+
+public class ClickUpProperties {
+
+    private static final String TOKEN = "clickup.token";
+    private static final String TEAM_ID = "clickup.team.id";
+
+    public static String getToken() {
+        if (getProperty(TOKEN).isEmpty() || getProperty(TOKEN).startsWith("YOUR")) {
+            return System.getProperty("TOKEN");
+        } else {
+            return getProperty(TOKEN);
+        }
+    }
+
+    public static String getTeamId() {
+        if (getProperty(TEAM_ID).isEmpty() || getProperty(TEAM_ID).startsWith("YOUR")) {
+            return System.getProperty("TEAM_ID");
+        } else {
+            return getProperty(TEAM_ID);
+        }
+    }
+
+    private static String getProperty(String key) {
+        return ResourceBundle.getBundle("clickup").getString(key);
+    }
+}
